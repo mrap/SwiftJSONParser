1ebe6db80e6800283e63d4bbf99b4117e858e623
Mike Rapadas <mike@mrap.me>
2014-08-27 16:00:08 -0700

    Initial Commit

---
+++ SwiftJSON/SwiftJSON.h
@@ -0,0 +1,19 @@
+//
+//  SwiftJSON.h
+//  SwiftJSON
+//
+//  Created by mrap on 8/27/14.
+//  Copyright (c) 2014 Mike Rapadas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftJSON.
+FOUNDATION_EXPORT double SwiftJSONVersionNumber;
+
+//! Project version string for SwiftJSON.
+FOUNDATION_EXPORT const unsigned char SwiftJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftJSON/PublicHeader.h>
+
+
